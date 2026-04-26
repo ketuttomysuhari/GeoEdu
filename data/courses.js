17848226f5a793ec6846b355d51c6dabfcd414ff
Ketut Tomy Suhari <51289099+ketuttomysuhari@users.noreply.github.com>
2026-04-27 01:45:22 +0800

    Create courses.js

---
+++ data/courses.js
@@ -0,0 +1,19 @@
+const courses = [
+{
+id:"kkg",
+name:"Kerangka Kontrol Geodesi",
+desc:"Jaringan kontrol horizontal, vertikal, adjustment, GNSS."
+},
+
+{
+id:"fotogrametri",
+name:"Fotogrametri",
+desc:"Pemodelan 3D, UAV, orientasi foto."
+},
+
+{
+id:"kartografi",
+name:"Kartografi Digital",
+desc:"Desain peta modern dan visualisasi."
+}
+];
